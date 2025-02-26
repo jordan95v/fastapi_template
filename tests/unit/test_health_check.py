dd565e7c42aef5eeb267437e6cd5ba5c8734d2e6
jordan95v <90615280+jordan95v@users.noreply.github.com>
2025-02-26 12:04:45 +0100

    Fixed health_check test.

--- tests/unit/test_health_check.py
+++ tests/unit/test_health_check.py
@@ -6,4 +6,4 @@ class TestHealthCheck:
     def test_health_check(self, client: TestClient) -> None:
         response: Response = client.get("/health_check")
         assert response.status_code == 200
-        assert response.json() == dict(status="Server is running")
+        assert response.json() == dict(status="Server is up")
