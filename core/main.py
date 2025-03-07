a25dab018edea242994145ff1bf442cbf8220865
jordan95v <90615280+jordan95v@users.noreply.github.com>
2025-03-07 22:57:20 +0100

    Remove lifespan context manager and add Alembic configuration for database migrations

--- core/main.py
+++ core/main.py
@@ -1,30 +1,8 @@
-from contextlib import asynccontextmanager
-from typing import AsyncGenerator
 from fastapi import FastAPI, Request
-
-from core.config import ENGINE
-from core.models.base import Base
 
 __all__: list[str] = ["app"]
 
-
-@asynccontextmanager
-async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
-    """Lifespan context manager for the FastAPI application.
-
-    This context manager is used to manage the lifespan of the FastAPI application.
-    It creates all the database tables defined in the Base metadata using the ENGINE
-    before yielding control back to the application.
-
-    Args:
-        app: The FastAPI application instance.
-    """
-
-    Base.metadata.create_all(ENGINE)
-    yield
-
-
-app: FastAPI = FastAPI(lifespan=lifespan)
+app: FastAPI = FastAPI()
 
 
 @app.get("/healthcheck", tags=["healthcheck"])
